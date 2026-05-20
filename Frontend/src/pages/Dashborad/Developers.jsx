b74de72520f2204b53d33ad8dbe8c7594450ea87
Mohamed Elsafy <your-email@example.com>
2026-05-20 19:23:42 +0300

    delete px-5

--- Frontend/src/pages/Dashborad/Developers.jsx
+++ Frontend/src/pages/Dashborad/Developers.jsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom";
 
 export default function Developers() {
   return ( 
-    <section className="py-4 px-5">
+    <section className="py-4">
       <Outlet
         context={{ title: "Developers", buttonText: "Add New Developer" }}
       />
