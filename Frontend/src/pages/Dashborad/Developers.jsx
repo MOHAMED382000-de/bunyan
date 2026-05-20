0d66b1689835e4489a407adf48edc9c2401328e0
Mohamed Elsafy <your-email@example.com>
2026-05-20 19:23:20 +0300

    update

--- Frontend/src/pages/Dashborad/Developers.jsx
+++ Frontend/src/pages/Dashborad/Developers.jsx
@@ -1,8 +1,8 @@
 import { Outlet } from "react-router-dom";
 
 export default function Developers() {
-  return (
-    <section className="py-4">
+  return ( 
+    <section className="py-4 px-5">
       <Outlet
         context={{ title: "Developers", buttonText: "Add New Developer" }}
       />
